7725cac19753bb4fad6996451e385a0da88e4888
nicolas <nhomble@terpmail.umd.edu>
2018-07-21 15:11:45 -0400

    ehh make it more specific

--- lux/services/clients/__init__.py
+++ lux/services/clients/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from lux.services.clients.groupme import GroupmeBot
 
-GROUPME_BOT_V = "GROUPME_BOT_ID"
+GROUPME_BOT_V = "GROUPME_LUX_BOT_ID"
 
-GROUPME_ID = os.environ.get(GROUPME_BOT_V, "")
+GROUPME_ID = os.environ.get(GROUPME_BOT_V, None)
 GROUPME_BOT = GroupmeBot(GROUPME_ID)
